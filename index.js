22f7cef481cd7549d56a81fea8b0fb10cc02a565
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-01 23:42:26 +0300

    looping using for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const messages = [];
+
+function writeCards (name, eventName) {
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+        //console.log(messages)
+    }
+    console.log(messages)
+    return messages;
+}
+
+function countDown(number) {
+    let num = number;
+     while (num >= 0) {
+        console.log(num)
+        num --;
+     }
+}
+
+countDown(40)
